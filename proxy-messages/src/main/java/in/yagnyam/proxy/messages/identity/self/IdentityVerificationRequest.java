1f178e4685d52f61cfa7a760961310f96b7034e4
Chenna Reddy <chenna@alavala.in>
2018-08-01 11:01:32 +0200

    added message for self verification

---
+++ proxy-messages/src/main/java/in/yagnyam/proxy/messages/identity/self/IdentityVerificationRequest.java
@@ -0,0 +1,75 @@
+package in.yagnyam.proxy.messages.identity.self;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import in.yagnyam.proxy.ProxyId;
+import in.yagnyam.proxy.SignableRequestMessage;
+import in.yagnyam.proxy.utils.StringUtils;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
+
+/**
+ * Request Message to Verify Subject by the Subject
+ */
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@ToString
+@JsonRootName("in.yagnyam.proxy.messages.identity.self.IdentityVerificationRequest")
+public class IdentityVerificationRequest implements SignableRequestMessage {
+
+  /**
+   * Unique Request Id
+   */
+  @NonNull
+  private String requestId;
+
+  /**
+   * Owner of this Identity.
+   * <p>
+   * Only owner is allowed to Update the identity details
+   */
+  @NonNull
+  private ProxyId ownerProxyId;
+
+  /**
+   * Subject being verified
+   */
+  @NonNull
+  private String subjectId;
+
+  /**
+   * Self verification.
+   */
+  @NonNull
+  private Boolean verified;
+
+  @Override
+  public String requestId() {
+    return requestId;
+  }
+
+  @Override
+  public ProxyId signer() {
+    return ownerProxyId;
+  }
+
+  @Override
+  public String toReadableString() {
+    return null;
+  }
+
+  @Override
+  @JsonIgnore
+  public boolean isValid() {
+    return StringUtils.isValid(requestId)
+        && ownerProxyId != null && ownerProxyId.isValid()
+        && StringUtils.isValid(subjectId);
+  }
+}
