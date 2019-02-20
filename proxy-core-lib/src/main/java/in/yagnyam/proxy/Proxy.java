139694be676af24a4c5f200ad30859fb89d197b0
Chenna Reddy <chenna@alavala.in>
2019-02-20 18:55:09 +0100

    adding json capability to proxy

--- proxy-core-lib/src/main/java/in/yagnyam/proxy/Proxy.java
+++ proxy-core-lib/src/main/java/in/yagnyam/proxy/Proxy.java
@@ -1,14 +1,22 @@
 package in.yagnyam.proxy;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.ToString;
 
 @Builder
 @Getter
-@ToString(of = {"id", "name"})
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(of = "id")
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Proxy {
 
   @NonNull
