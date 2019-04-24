a3a3f2e540704fb951c20fdc9e58ca056398309c
Chenna Reddy <chenna@alavala.in>
2019-04-24 22:20:23 +0200

    simplyfied deposit request message

--- proxy-messages/src/main/java/in/yagnyam/proxy/messages/banking/DepositRequest.java
+++ proxy-messages/src/main/java/in/yagnyam/proxy/messages/banking/DepositRequest.java
@@ -7,7 +7,14 @@ import in.yagnyam.proxy.ProxyId;
 import in.yagnyam.proxy.SignableRequestMessage;
 import in.yagnyam.proxy.SignedMessage;
 import in.yagnyam.proxy.utils.StringUtils;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
 
 
 /**
@@ -21,75 +28,79 @@ import lombok.*;
 @EqualsAndHashCode(of = {"depositId"})
 public class DepositRequest implements SignableRequestMessage, AddressableMessage {
 
-    @Builder
-    @NoArgsConstructor(access = AccessLevel.PRIVATE)
-    @AllArgsConstructor(access = AccessLevel.PRIVATE)
-    @Getter
-    @ToString
-    public static class RequestingCustomer {
+  @Builder
+  @NoArgsConstructor(access = AccessLevel.PRIVATE)
+  @AllArgsConstructor(access = AccessLevel.PRIVATE)
+  @Getter
+  @ToString
+  public static class RequestingCustomer {
 
-        private String name;
+    private String name;
 
-        private String phone;
+    private String phone;
 
-        private String email;
+    private String email;
 
-    }
-
-    @NonNull
-    private String depositId;
-
-    @NonNull
-    public SignedMessage<ProxyAccount> proxyAccount;
-
-    @NonNull
-    private String accountName;
-
-    @NonNull
-    private Amount amount;
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String message;
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private RequestingCustomer requestingCustomer;
-
-    @Override
-    public ProxyId address() {
-        return proxyAccount.getSignedBy();
-    }
-
-    @Override
-    public String requestId() {
-        return depositId;
-    }
-
-    @Override
-    public ProxyId signer() {
-        return proxyAccount.getMessage().getOwnerProxyId();
-    }
-
-    @Override
-    public String toReadableString() {
-        return null;
-    }
-
-    @Override
-    @JsonIgnore
     public boolean isValid() {
-        return StringUtils.isValid(depositId)
-                && proxyAccount != null && proxyAccount.isValid()
-                && StringUtils.isValid(accountName)
-                && amount != null && amount.isValid();
+      return StringUtils.isValid(name) && StringUtils.isValid(phone) && StringUtils.isValid(email);
     }
 
-    @JsonIgnore
-    public ProxyAccountId getProxyAccountId() {
-        return proxyAccount != null && proxyAccount.getMessage() != null ? proxyAccount.getMessage().getProxyAccountId() : null;
-    }
+  }
 
-    @JsonIgnore
-    public ProxyId getOwnerProxyId() {
-        return proxyAccount != null && proxyAccount.getMessage() != null ? proxyAccount.getMessage().getOwnerProxyId() : null;
-    }
+  @NonNull
+  private String depositId;
+
+  @NonNull
+  public SignedMessage<ProxyAccount> proxyAccount;
+  @NonNull
+  private Amount amount;
+
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  @NonNull
+  private String message;
+
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  private RequestingCustomer requestingCustomer;
+
+  @Override
+  public ProxyId address() {
+    return proxyAccount.getSignedBy();
+  }
+
+  @Override
+  public String requestId() {
+    return depositId;
+  }
+
+  @Override
+  public ProxyId signer() {
+    return proxyAccount.getMessage().getOwnerProxyId();
+  }
+
+  @Override
+  public String toReadableString() {
+    return null;
+  }
+
+  @Override
+  @JsonIgnore
+  public boolean isValid() {
+    return StringUtils.isValid(depositId)
+        && proxyAccount != null && proxyAccount.isValid()
+        && StringUtils.isValid(message)
+        && amount != null && amount.isValid()
+        && (requestingCustomer == null || requestingCustomer.isValid());
+  }
+
+  @JsonIgnore
+  public ProxyAccountId getProxyAccountId() {
+    return proxyAccount != null && proxyAccount.getMessage() != null ? proxyAccount.getMessage()
+        .getProxyAccountId() : null;
+  }
+
+  @JsonIgnore
+  public ProxyId getOwnerProxyId() {
+    return proxyAccount != null && proxyAccount.getMessage() != null ? proxyAccount.getMessage()
+        .getOwnerProxyId() : null;
+  }
 }
